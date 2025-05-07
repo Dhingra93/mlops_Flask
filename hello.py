3b4c6cabbb7d6552a4f770f082f1d4c0c13cb88d
Dhingra93 <rohitdhingra93@yahoo.com>
2025-05-07 19:41:37 +0530

    Add

--- hello.py
+++ hello.py
@@ -1,6 +1,45 @@
 from flask import Flask, request,jsonify
 
+import joblib
+
+clf=joblib.load('classifier.pkl')
+
+
+
 app=Flask(__name__)
+
+@app.route('/predict',methods=['POST'])
+def predict(): 
+     loan_req=request.get_json()
+     if loan_req['Gender']== 'Male':
+         Gender = 1
+     else :
+         Gender = 0
+
+     if loan_req['Married']== 'Married':
+         Married = 1
+     else :
+         Married = 0
+
+     if loan_req['Dependents']== '3+':
+         Dependents = 3
+
+     ApplicantIncome=loan_req['ApplicantIncome']
+     LoanAmount=loan_req['LoanAmount']
+     Credit_History=loan_req['Credit_History']
+
+     result=clf.predict('Gender','Married','Dependents','ApplicantIncome','LoanAmount','Credit_History')
+
+     if result==0:
+        result='Loan Approved'
+     else:
+         result='Loan Rejected'
+
+     return {'Loan status': result}
+     
+     
+      
+      
 
 @app.route('/hello',methods=['GET'])
 def hello():
